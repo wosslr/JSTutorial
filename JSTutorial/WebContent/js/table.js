3b15eecdd7926176e868cbcd22de8bb6049540ae
wosslr <wosslr@163.com>
2015-06-07 01:11:48 +0800

    demo1

---
+++ JSTutorial/WebContent/js/table.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+function doit(){
+	document.getElementById("demo").innerHTML="My First JavaScript Function 111";
+	var x = 1;
+	var y = 4;
+	var z = x + y;
+	document.getElementById("demo2").innerHTML=z;
+}
