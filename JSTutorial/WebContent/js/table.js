690ddfbf35b66e2fee0c584f1a4a54a6dbfceac1
wosslr <wosslr@163.com>
2015-06-12 00:35:43 +0800

    20150612

--- JSTutorial/WebContent/js/table.js
+++ JSTutorial/WebContent/js/table.js
@@ -2,10 +2,19 @@
  * 
  */
 
-function doit(){
-	document.getElementById("demo").innerHTML="My First JavaScript Function 111";
-	var x = 1;
-	var y = 4;
-	var z = x + y;
-	document.getElementById("demo2").innerHTML=z;
+function createTable(message){
+	
+	for(var i=0; i<10;i++){
+		
+		var row = document.createElement("tr");
+		
+		for(var j = 0; j < 10; j++){
+			var cell = document.createElement("td");
+			cell.appendChild(document.createTextNode("test"));
+			row.appendChild(cell);
+		}
+		var oTable = document.getElementById("newbody");
+		document.getElementById("newbody").appendChild(row);
+	}
+	
 }
