f36aa8f395bf7f18522fd8e526997b936a3f72a9
wosslr <wosslr@163.com>
2015-06-23 23:47:40 +0800

    20150623

--- JSTutorial/WebContent/js/table.js
+++ JSTutorial/WebContent/js/table.js
@@ -2,114 +2,97 @@
  * 
  */
 
-var salesOrders = [
-      {
-    	  ID: "1527",
-    	  customer: "101",
-    	  date: "2015-06-10",
-    	  status: "Released",
-    	  items: [
-    	          {
-    	        	  itemID: "10",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 100,
-    	        	  unit: "EA"
-    	          },
-    	          {
-    	        	  itemID: "20",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 150,
-    	        	  unit: "EA"
-    	          }
-    	  ]
-      },
-      {
-    	  ID: "1528",
-    	  customer: "101",
-    	  date: "2015-06-11",
-    	  status: "In Process",
-    	  items: [
-    	          {
-    	        	  itemID: "10",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 20,
-    	        	  unit: "EA"
-    	          },
-    	          {
-    	        	  itemID: "20",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 50,
-    	        	  unit: "EA"
-    	          }
-    	  ]
-      },
-      {
-    	  ID: "1529",
-    	  customer: "102",
-    	  date: "2015-06-12",
-    	  status: "New",
-    	  items: [
-    	          {
-    	        	  itemID: "10",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 23,
-    	        	  unit: "EA"
-    	          },
-    	          {
-    	        	  itemID: "20",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 45,
-    	        	  unit: "EA"
-    	          }
-    	  ]
-      },
-      {
-    	  ID: "1530",
-    	  customer: "102",
-    	  date: "2015-06-13",
-    	  status: "New",
-    	  items: [
-    	          {
-    	        	  itemID: "10",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 23,
-    	        	  unit: "EA"
-    	          },
-    	          {
-    	        	  itemID: "20",
-    	        	  product: "MCF-0001",
-    	        	  quantity: 45,
-    	        	  unit: "EA"
-    	          }
-    	  ]
-      },
-];
+var salesOrders = [ {
+	ID : "1527",
+	customer : "101",
+	date : "2015-06-10",
+	status : "Released",
+	items : [ {
+		itemID : "10",
+		product : "MCF-0001",
+		quantity : 100,
+		unit : "EA"
+	}, {
+		itemID : "20",
+		product : "MCF-0001",
+		quantity : 150,
+		unit : "EA"
+	} ]
+}, {
+	ID : "1528",
+	customer : "101",
+	date : "2015-06-11",
+	status : "In Process",
+	items : [ {
+		itemID : "10",
+		product : "MCF-0001",
+		quantity : 20,
+		unit : "EA"
+	}, {
+		itemID : "20",
+		product : "MCF-0001",
+		quantity : 50,
+		unit : "EA"
+	} ]
+}, {
+	ID : "1529",
+	customer : "102",
+	date : "2015-06-12",
+	status : "New",
+	items : [ {
+		itemID : "10",
+		product : "MCF-0001",
+		quantity : 23,
+		unit : "EA"
+	}, {
+		itemID : "20",
+		product : "MCF-0001",
+		quantity : 45,
+		unit : "EA"
+	} ]
+}, {
+	ID : "1530",
+	customer : "102",
+	date : "2015-06-13",
+	status : "New",
+	items : [ {
+		itemID : "10",
+		product : "MCF-0001",
+		quantity : 23,
+		unit : "EA"
+	}, {
+		itemID : "20",
+		product : "MCF-0001",
+		quantity : 45,
+		unit : "EA"
+	} ]
+}, ];
 
 var gSOStatus = {
-	NEW: "New",	
-	IN_PROCESS: "In Process",
-	RELEASED: "Released",
-	CANCELLED: "Cancelled",
+	NEW : "New",
+	IN_PROCESS : "In Process",
+	RELEASED : "Released",
+	CANCELLED : "Cancelled",
 };
 
 var gSelectedRow;
 
-function createTable(message){
-	
+function createTable(message) {
+
 	var hRow = document.createElement("tr");
 	hRow.style.width = "200%";
-	
+
 	appendCell(hRow, "owlCell", "th", "Order ID");
 	appendCell(hRow, "owlCell", "th", "Customer");
 	appendCell(hRow, "owlCell", "th", "Status");
 	appendCell(hRow, "owlCell", "th", "Date");
 	document.getElementById("newtitle").appendChild(hRow);
-	
-	for(var i=0; i<salesOrders.length;i++){
-		
+
+	for (var i = 0; i < salesOrders.length; i++) {
+
 		var row = document.createElement("tr");
-		row.onclick = function (){
-			
+		row.onclick = function() {
+
 			if (gSelectedRow === this) {
 				gSelectedRow = null;
 				this.className = "";
@@ -123,16 +106,16 @@ function createTable(message){
 			}
 			resetActButtonStatus();
 		};
-		
+
 		appendCell(row, "owlCell", "td", salesOrders[i].ID);
 		appendCell(row, "owlCell", "td", salesOrders[i].customer);
 		appendCell(row, "owlCell", "td", salesOrders[i].status);
-		
+
 		var date = new Date(salesOrders[i].date);
 		appendCell(row, "owlCell", "td", date.toDateString());
 		document.getElementById("newbody").appendChild(row); //
 	}
-	
+
 	resetActButtonStatus();
 }
 
@@ -143,36 +126,40 @@ function appendCell(oRow, className, cellTag, text) {
 	oRow.appendChild(oCell);
 }
 
-function showNewSOpup(w,h){   
-    var popUnder = document.getElementById("newSOPopUnder");
-    popUnder.style.top = "0px";
-    popUnder.style.left = "0px";
-    popUnder.style.width = "100%";
-    popUnder.style.height = "100%";
-    popUnder.style.visibility = "visible";
-    popUnder.style.display = "block";
-    
-    var popup = document.getElementById("newSOPop");
-    popup.style.visibility = "visible";
-    popup.style.left = "40%";
-    popup.style.top = "30%";    
-    //document.body.style.margin = 0;
+function showNewSOpup() {
+	$("#newSOPopUnder").css({
+		"top" : "0px",
+		"left" : "0px",
+		"width" : "100%",
+		"height" : "100%",
+		"visibility" : "visible",
+		"display" : "block",
+	});
+
+	$("#newSOPop").css({
+		"top" : "30%",
+		"left" : "40%",
+		"visibility" : "visible",
+		"display" : "block",
+	});
+
+	$("#newSOPop").load("view/NewSalesOrder.html");
 }
 
-function hidepopup(){   
-    var popup = document.getElementById("newSOPopUnder");   
-    popup.style.visibility = "hidden";
+function hidepopup() {
+	var popup = document.getElementById("newSOPopUnder");
+	popup.style.visibility = "hidden";
 }
 
-function deleteRow(){
-	if(gSelectedRow != null){
+function deleteRow() {
+	if (gSelectedRow != null) {
 		document.getElementById("newbody").deleteRow(gSelectedRow.rowIndex - 1);
 		gSelectedRow = null;
 	}
 	resetActButtonStatus();
 }
 
-function setActButtonEnable( sProcess, sRelease, sCancel ) {
+function setActButtonEnable(sProcess, sRelease, sCancel) {
 	document.getElementById("btnProcess").enable = sProcess;
 	document.getElementById("btnProcess").disabled = !sProcess;
 	document.getElementById("btnRelease").enable = sRelease;
@@ -184,11 +171,11 @@ function setActButtonEnable( sProcess, sRelease, sCancel ) {
 function resetActButtonStatus() {
 	if (gSelectedRow == null) {
 		setActButtonEnable(false, false, false);
-	}else if (gSelectedRow.childNodes[2].innerText == gSOStatus.NEW) {
+	} else if (gSelectedRow.childNodes[2].innerText == gSOStatus.NEW) {
 		setActButtonEnable(true, false, true);
-	}else if (gSelectedRow.childNodes[2].innerText == gSOStatus.IN_PROCESS) {
+	} else if (gSelectedRow.childNodes[2].innerText == gSOStatus.IN_PROCESS) {
 		setActButtonEnable(false, true, true);
-	}else{
+	} else {
 		setActButtonEnable(false, false, false);
 	}
 }
